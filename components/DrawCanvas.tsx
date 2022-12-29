21bc8f53475448518fe83afabafd68720abb6a50
Jaime <alu0101329093@ull.edu.es>
2022-12-29 11:59:54 +0000

    feat: Add predict button functionality

--- components/DrawCanvas.tsx
+++ components/DrawCanvas.tsx
@@ -7,9 +7,17 @@ import { useDraw } from '../hooks/useDraw';
 export interface DrawCanvasProps {
   clear: boolean;
   resetClear: () => void;
+  predict: boolean;
+  resetPredict: () => void;
+  setPrediction: (prediction: string) => void;
 }
 
-const DrawCanvas: FC<DrawCanvasProps> = ({ clear, resetClear }) => {
+const DrawCanvas: FC<DrawCanvasProps> = ({
+  clear,
+  resetClear,
+  predict,
+  resetPredict,
+}) => {
   const canvasRef = useRef(null);
   const parentRef = useRef(null);
   const [clearHandler] = useDraw(canvasRef, parentRef);
@@ -21,11 +29,17 @@ const DrawCanvas: FC<DrawCanvasProps> = ({ clear, resetClear }) => {
     }
   }, [clear, clearHandler, resetClear]);
 
+  useEffect(() => {
+    if (predict) {
+      // TODO: Try to predict using model
+      resetPredict();
+    }
+  });
+
   return (
     <div
       className="h-4/6 w-8/12 rounded-3xl border-8 border-black bg-white shadow-[1px_4px_10px_5px_rgba(0,0,0,0.25)]"
       ref={parentRef}
-      // onResize={}
     >
       <canvas height={600} ref={canvasRef} width={1280}></canvas>
     </div>
--- components/UserInputZone.tsx
+++ components/UserInputZone.tsx
@@ -6,24 +6,32 @@ import OptionButton from './OptionButton';
 
 const UserInputZone: FC = () => {
   const [clear, setClear] = useState<boolean>(false);
+  const [predict, setPredict] = useState<boolean>(false);
+  const [prediction, setPrediction] = useState<string>('...');
 
-  const clearClickHandler = (): void => {
-    setClear(true);
-  };
+  const clearClickHandler = (): void => setClear(true);
 
-  const resetClearHandler = (): void => {
-    setClear(false);
-  };
+  const resetClearHandler = (): void => setClear(false);
+
+  const predictClickHandler = (): void => {};
+
+  const resetPredictHandler = (): void => setPredict(false);
 
   return (
     <>
-      <DrawCanvas clear={clear} resetClear={resetClearHandler} />
+      <DrawCanvas
+        clear={clear}
+        resetClear={resetClearHandler}
+        predict={predict}
+        resetPredict={resetPredictHandler}
+        setPrediction={setPrediction}
+      />
       <span className="w-1/2 rounded-xl border-4 border-black bg-white p-3 text-center text-3xl font-semibold">
-        This is {'...'}
+        This is {prediction}
       </span>
       <div className="flex w-7/12 justify-center text-3xl font-semibold">
         <OptionButton>Import</OptionButton>
-        <OptionButton>Predict</OptionButton>
+        <OptionButton onClick={predictClickHandler}>Predict</OptionButton>
         <OptionButton onClick={clearClickHandler}>Clear</OptionButton>
       </div>
     </>
